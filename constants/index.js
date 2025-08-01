ef0c6a16d77dcec2403387a97fac9f5d8ea14f0f
NjilaBoris <njilaboris2@gmail.com>
2025-08-01 01:59:50 +0100

    Final

---
+++ constants/index.js
@@ -0,0 +1,18 @@
+export const TABS = [
+  {
+    name: "Payments",
+    icon: "💳", // Credit card emoji for Payments
+  },
+  {
+    name: "Balances",
+    icon: "💰", // Money bag emoji for Balances
+  },
+  {
+    name: "Customers",
+    icon: "👥", // People emoji for Customers
+  },
+  {
+    name: "Billing",
+    icon: "🧾", // Receipt emoji for Billing
+  },
+];
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import { TABS } from "../constants";
+import { motion } from "motion/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [hovered, setHovered] = useState(0);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="h-dvh w-full flex justify-center items-center">
+      <nav className="flex items-center gap-8">
+        {TABS.map((tab, index) => (
+          <a
+            href={tab.name}
+            className="text-[1.2rem] relative cursor-pointer p-2  hover:text-neutral-200 transition-colors"
+            key={index}
+            onMouseEnter={() => setHovered(index)}
+            onMouseLeave={() => setHovered(0)}
+          >
+            {hovered === index && (
+              <motion.span
+                layoutId="hovered-span"
+                className="h-full  absolute inset-0 rounded-md bg-[#2090FF]"
+              />
+            )}
+            <span className="relative z-10">
+              {tab.icon} {tab.name}
+            </span>
+          </a>
+        ))}
+      </nav>
+    </div>
+  );
+};
 
-export default App
+export default App;
